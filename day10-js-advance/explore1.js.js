b5425336a631153ac11176c053f594ae6708c57b
Aayush Saini <aayushsaini0406@gmail.com>
2025-07-15 21:41:55 +0530

    JS Dom Completed

---
+++ day10-js-advance/explore1.js.js
@@ -0,0 +1,12 @@
+console.log("chai aur code");
+
+function sayHello() {
+  console.log("I would like to say Hello");
+}
+setTimeout(() => {
+  sayHello();
+}, 2000);
+
+for (let index = 0; index < 10; index++) {
+  console.log(index);
+}
--- day9-JS/JS-DOM/challenges.js
+++ day9-JS/JS-DOM/challenges.js
@@ -1,1 +1,76 @@
-// alert("connected");
+// example 1
+document
+  .getElementById("changeTextButton")
+  .addEventListener("click", function () {
+    // arrow functions are not used here to write
+    let paragraph = document.getElementById("myParagraph");
+    paragraph.textContent = "the paragraph is changed";
+  });
+
+//example2
+document
+  .getElementById("highlightFirstCity")
+  .addEventListener("click", function () {
+    let citiesList = document.getElementById("citiesList");
+    citiesList.firstElementChild.classList.add("highlight");
+  });
+
+//example3
+document.getElementById("changeOrder").addEventListener("click", function () {
+  let order = document.getElementById("coffeeType");
+  order.textContent = "Espresso";
+  order.style.backgroundColor = "brown";
+});
+
+//example4
+document.getElementById("addNewItem").addEventListener("click", function () {
+  let item = document.createElement("li");
+  item.textContent = `jam`;
+  document.getElementById("shoppingList").appendChild(item);
+});
+
+//example5
+document
+  .getElementById("removeLastTask")
+  .addEventListener("click", function () {
+    let task = document.getElementById("taskList");
+    task.lastChild.remove();
+  });
+
+//example6
+document
+  .getElementById("clickMeButton")
+  .addEventListener("mouseover", function () {
+    alert("chai");
+  });
+
+//example7
+document.getElementById("teaList").addEventListener("click", function (event) {
+  if (event.target && event.target.matches(".teaItem")) {
+    alert("you selected" + event.target.textContent);
+  }
+});
+
+//example8
+document
+  .getElementById("feedbackForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    let feedback = document.getElementById("feedbackInput").value ;
+    console.log(feedback);
+    document.getElementById(
+      "feedbackDisplay"
+    ).textContent = `Feedback is: ${feedback}`;
+  });
+
+//example9
+document.addEventListener('DOMContentLoaded', function(){
+    document.getElementById('domStatus').textContent = "DOM fully loaded"
+})
+
+//example10
+document.getElementById("toggleHighlight").addEventListener('click', function(){
+    let text = document.getElementById("descriptionText");
+    // text.classList.add('highlight');
+    text.style.color ="green";
+} )
