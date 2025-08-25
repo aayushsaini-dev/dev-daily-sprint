110af0ee696bcb5dd44b2aff4cee1b4d4e64c428
Aayush Saini <aayushsaini0406@gmail.com>
2025-08-25 23:21:31 +0530

    fullstack auth System begin

---
+++ day32-fullstack-Authetication-system/auth-system/src/app/login/page.tsx
@@ -0,0 +1,44 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+import { useRouter } from "next/navigation";
+import { axios } from "axios";
+
+export default function LoginPage() {
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "",
+
+  });
+
+  const onLogin = async () => {};
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="text-center text-white bg-green-400">Login</h1>
+      <hr />
+    
+      <label htmlFor="email">email</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focous:outline-none focous:border-gray-600"
+        type="text"
+        id="email"
+        value={user.email}
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
+        placeholder="Email"
+      />
+      <label htmlFor="password">password</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focous:outline-none focus:border-gray-600"
+        type="password"
+        id="password"
+        value={user.password}
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+        placeholder="Password"
+      />
+      <button
+      onClick={onLogin}
+      className="p-2 border border-gray-300 rounded-lg mb-4 focous:outline-none focus:border-gray-600">Login here</button>
+      <Link href="/signup">Visit Signup page </Link>
+    </div>
+  );
+}
